b3ef6a52657f39e3967580f2b984547ac411731c
Mobola Falomo <mobolafalomo@gmail.com>
2022-11-24 13:59:10 -0800

    printf updated

--- _printf.c
+++ _printf.c
@@ -3,10 +3,10 @@
 void print_buffer(char buffer[], int *buff_ind);
 
 /**
-  * _printf - function that produces output according to a format
-  * @format: char string
-  * Return: the number of char printed
-  */
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
+ */
 
 int _printf(const char *format, ...)
 {
@@ -27,7 +27,7 @@ int _printf(const char *format, ...)
 			buffer[buff_ind++] = format[i];
 			if (buff_ind == BUFF_SIZE)
 				print_buffer(buffer, &buff_ind);
-			/* write(1, &format[i], 1); */
+			/* write(1, &format[i], 1);*/
 			printed_chars++;
 		}
 		else
@@ -38,8 +38,8 @@ int _printf(const char *format, ...)
 			precision = get_precision(format, &i, list);
 			size = get_size(format, &i);
 			++i;
-			printed = handle_print(format, &i, list, buffer, flags
-					width, precision, size);
+			printed = handle_print(format, &i, list, buffer,
+					flags, width, precision, size);
 			if (printed == -1)
 				return (-1);
 			printed_chars += printed;
@@ -53,10 +53,10 @@ int _printf(const char *format, ...)
 }
 
 /**
-  * print_buffer - prints the contents of the buffer if it exist
-  * @buffer: array of chars
-  * @buff_ind: Index at which to add next char, represents the length
-  */
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
 
 void print_buffer(char buffer[], int *buff_ind)
 {
