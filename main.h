7fee6c0f2b657af0e94c694a46593f30097b0bd0
Mobola Falomo <mobolafalomo@gmail.com>
2022-11-26 06:36:52 -0800

    printf updated

--- main.h
+++ main.h
@@ -111,8 +111,5 @@ int is_digit(char);
 long int convert_size_number(long int num, int size);
 long int convert_size_unsgnd(unsigned long int num, int size);
 
-char *stringify(int n);
-
-int formatter(const char *format, va_list args);
 
 #endif /* MAIN_H */
