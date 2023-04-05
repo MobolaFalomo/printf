000241e78d33da04afe4a7d2c1bd0fe823906d71
MrBarns <barnabasoluwatosin@gmail.com>
2023-04-05 16:15:25 +0100

    Task0 modify str function

--- char_printers.c
+++ char_printers.c
@@ -29,16 +29,18 @@ int print_char(va_list vlist)
 int print_str(va_list vlist)
 {
 	char *buf;
-	int len;
+	int ind, count = 0;
 
 	buf = va_arg(vlist, char *);
 	if (buf == NULL)
 		buf = "(null)";
 
-	for (len = 0; buf[len] != '\0'; len++)
-		write(1, &buf[len], 1);
-
-	return (len);
+	for (ind = 0; buf[ind] != '\0'; ind++)
+	{
+		write(1, &buf[ind], 1);
+		count++;
+	}
+	return (count);
 }
 
 
